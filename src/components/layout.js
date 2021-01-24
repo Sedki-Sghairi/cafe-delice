a639130f3f9c8ea4622075372d17a3c523a5baa9
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-24 12:08:57 +0400

    bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,13 @@
 
-
 import React from "react"
-
-// import { useStaticQuery, graphql } from "gatsby"
+import './bootstrap.min.css'
+// import './loyout.css'
 
 const Layout = ({ children }) => {
-
   return (
     <>
         <h1>hello world</h1>
+        {children}
     </>
   )
 }
