5c97215f435528ef474d7cdb4529dd55e2f674f8
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-25 22:28:41 +0400

    back to function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,44 +2,43 @@ import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { Link } from 'gatsby';
 import { FaCoffee } from 'react-icons/fa';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export default class Navbar extends Component {
-	state = {
-		isOpen: false,
-		collapse: 'collapse navbar-collapse'
+const Navbar = () => {
+	const [ isOpen, setIsOpen ] = useState(false);
+	const toggleHandler = () => {
+		setIsOpen(!isOpen);
 	};
-	toggleHandler = () => {};
-	render() {
-		return (
-			<nav className="navbar navbar-expand-md bg-light navbar-light">
-				<Link to="/" className="navbar-brand">
-					café Delice
-				</Link>
-				<FaCoffee className="fa-lg ml-auto mr-2" />
-				<button className="navbar-toggler" type="button" onClick={this.toggleHandler}>
-					<FaBars className="fa-lg" />
-				</button>
-				<div className={this.state.collapse}>
-					<ul className="navbar-nav mx-auto">
-						<li className="nav-item">
-							<Link to="/" className="nav-link">
-								HOME
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/aboutus" className="nav-link">
-								ABOUT US
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/services" className="nav-link">
-								OUR SERVICES
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		);
-	}
-}
+	const show = isOpen ? 'show' : '';
+	return (
+		<nav className="navbar navbar-expand-md bg-light navbar-light">
+			<Link to="/" className="navbar-brand">
+				café Delice
+			</Link>
+			<FaCoffee className="fa-lg ml-auto mr-2" />
+			<button className="navbar-toggler" type="button" onClick={toggleHandler}>
+				<FaBars className="fa-lg" />
+			</button>
+			<div className={`${show} collapse navbar-collapse`}>
+				<ul className="navbar-nav mx-auto">
+					<li className="nav-item">
+						<Link to="/" className="nav-link">
+							HOME
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/aboutus" className="nav-link">
+							ABOUT US
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/services" className="nav-link">
+							OUR SERVICES
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+export default Navbar;
